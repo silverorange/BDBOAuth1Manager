79e79b4fc4199d2bf829b446cd281ab206df6761
Michael Gauthier <mike@silverorange.com>
2014-01-02 02:49:31 -0400

    whitespace

--- BDBOAuth1Manager/BDBOAuth1SessionManager.m
+++ BDBOAuth1Manager/BDBOAuth1SessionManager.m
@@ -31,7 +31,7 @@
     self = [super initWithBaseURL:url];
     if (self)
     {
-        self.requestSerializer  = [BDBOAuth1RequestSerializer serializerForServiceAndRealm:url.host withConsumerKey:key consumerSecret:secret realm:realm];
+        self.requestSerializer = [BDBOAuth1RequestSerializer serializerForServiceAndRealm:url.host withConsumerKey:key consumerSecret:secret realm:realm];
     }
     return self;
 }
